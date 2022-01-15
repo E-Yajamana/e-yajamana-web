1d578b2613e7dd497c07d7475910b3a4f5cce6c0
Lidyayanti <60130006+Alsania14@users.noreply.github.com>
2022-01-15 17:47:48 +0800

    Update lupa-password.blade.php

--- resources/views/layouts/email/lupa-password.blade.php
+++ resources/views/layouts/email/lupa-password.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.email.template-lupa-password')
-@section('content')
+@section('email-content')
     <p>Masukkan code berikut ini pada form kode</p><br>
     <p style="font-size:20px;"><b>{{ $data['token'] }}</b></p>
 @endsection
