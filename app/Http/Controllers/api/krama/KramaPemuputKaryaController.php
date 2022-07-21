43f9f07c7e1d5c8ee62e51926b64ffca37541842
AlinGotama <60130006+Alsania14@users.noreply.github.com>
2022-07-21 23:20:54 +0800

    Fixing Filter Favorit

--- app/Http/Controllers/api/krama/KramaPemuputKaryaController.php
+++ app/Http/Controllers/api/krama/KramaPemuputKaryaController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\api\krama;
 
 use App\Http\Controllers\Controller;
+use App\Models\BanjarDinas;
 use App\Models\Favorit;
 use App\Models\GriyaRumah;
+use App\Models\PemuputKarya;
 use App\Models\Sanggar;
 use Doctrine\DBAL\Query\QueryException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -25,7 +27,10 @@ class KramaPemuputKaryaController extends Controller
         // SECURITY
         $validator = Validator::make($request->all(), [
             'status' => 'nullable|in:sulinggih,pemangku,sanggar',
+            'id_kabupaten' => 'nullable|numeric',
             'id_kecamatan' => 'nullable|numeric',
+            'id_desa_dinas' => 'nullable|numeric',
+            'id_banjar_dinas' => 'nullable|numeric',
             'isFavorit' => 'nullable|in:true,false'
         ]);
 
@@ -41,144 +46,125 @@ class KramaPemuputKaryaController extends Controller
         // MAIN LOGIC
         try {
             $user = Auth::user();
-            $griyaRumahs = GriyaRumah::query()->with(['PemuputKarya'])->whereHas('PemuputKarya');
-            $sanggars = Sanggar::query();
+            $griyaRumahs = GriyaRumah::with([
+                'PemuputKarya.User.Reservasi.DetailReservasi.TahapanUpacara',
+                'PemuputKarya.User.Penduduk',
+                'PemuputKarya.AtributPemuput',
+                'PemuputKarya.FavoritUser',
+            ]);
+            $sanggars = Sanggar::with([
+                'User',
+                'Reservasi.DetailReservasi.TahapanUpacara',
+                'FavoritUser',
+            ]);
 
-            if ($request->status != null && $request->status != "") {
-                if ($request->status == 'sanggar') {
-                    $griyaRumahs->where('id', -1);
-                    $sanggars->with([
-                        'User',
-                        'Reservasi' => function ($sanggarQuery) {
-                            $sanggarQuery->with([
-                                'DetailReservasi' => function ($detailReservasiQuery) {
-                                    $detailReservasiQuery->with('TahapanUpacara');
-                                }
-                            ]);
-                        }
-                    ])->whereHas('User');
-                } else {
-                    $sanggars->where('id', -1);
-
-                    $sulinggihQuery = function ($sulinggihQuery) use ($request, $user) {
-                        $sulinggihQuery
-                            ->with([
-                                'User' => function ($userQuery) {
-                                    $userQuery->with([
-                                        'Reservasi' => function ($reservasiQuery) {
-                                            $reservasiQuery->with([
-                                                'DetailReservasi' => function ($detailReservasiQuery) {
-                                                    $detailReservasiQuery->with('TahapanUpacara');
-                                                }
-                                            ]);
-                                        },
-                                        'Penduduk'
-                                    ])->whereHas('Penduduk');
-                                },
-                                'AtributPemuput',
-                                'FavoritUser' => function ($favoritUser) use ($user) {
-                                    $favoritUser->where('id_user', $user->id);
-                                }
-                            ])
-                            ->whereHas('User')
-                            ->whereHas('AtributPemuput')
-                            ->where('tipe', $request->status);
-                        // FAVORIT FILTER
-                        if ($request->isFavorit != null && $request->isFavorit != '') {
-                            $sulinggihQuery->whereHas(
-                                'FavoritUser',
-                                function ($favoritUser) use ($user) {
-                                    $favoritUser->where('id_user', $user->id);
-                                }
-                            );
-                        }
-                    };
-
-                    $griyaRumahs->with([
-                        'PemuputKarya' => $sulinggihQuery
-                    ])->whereHas('PemuputKarya', $sulinggihQuery);
-                }
-            } else {
-                $sanggars->with([
-                    'User',
-                    'Reservasi' => function ($sanggarQuery) {
-                        $sanggarQuery->with([
-                            'DetailReservasi' => function ($detailReservasiQuery) {
-                                $detailReservasiQuery->with('TahapanUpacara');
-                            }
-                        ]);
-                    }
-                ])->whereHas('User');
-                $sulinggihQuery = function ($sulinggihQuery) use ($request, $user) {
-                    $sulinggihQuery
-                        ->with([
-                            'User' => function ($userQuery) {
-                                $userQuery->with([
-                                    'Reservasi' => function ($reservasiQuery) {
-                                        $reservasiQuery->with([
-                                            'DetailReservasi' => function ($detailReservasiQuery) {
-                                                $detailReservasiQuery->with('TahapanUpacara');
-                                            }
-                                        ]);
-                                    },
-                                    'Penduduk'
-                                ])->whereHas('Penduduk');;
-                            },
-                            'AtributPemuput',
-                            'FavoritUser' => function ($favoritUser) use ($user) {
-                                $favoritUser->where('id_user', $user->id);
-                            }
-                        ])
-                        ->whereHas('User')
-                        ->whereHas('AtributPemuput');
-                    // FAVORIT FILTER
-                    if ($request->isFavorit != null && $request->isFavorit != '') {
-                        $sulinggihQuery->whereHas(
-                            'FavoritUser',
-                            function ($favoritUser) use ($user) {
-                                $favoritUser->where('id_user', $user->id);
-                            }
-                        );
-                    }
-                };
-
-                $griyaRumahs->with([
-                    'PemuputKarya' => $sulinggihQuery
-                ])->whereHas('PemuputKarya', $sulinggihQuery);
-            }
-
-            if ($request->id_kecamatan != null && $request->id_kecamatan != 0) {
-                // QUERY DESA
-                $banjarDinasQuery = function ($banjarDinasQuery) use ($request) {
-                    $banjarDinasQuery->with([
-                        'DesaDinas' => function ($desaDinasQuery) use ($request) {
-                            $desaDinasQuery->with([
-                                'Kecamatan' => function ($kecamatanQuery) use ($request) {
-                                    $kecamatanQuery->where('id', $request->id_kecamatan);
-                                }
-                            ])->whereHas('Kecamatan', function ($kecamatanQuery) use ($request) {
-                                $kecamatanQuery->where('id', $request->id_kecamatan);
-                            });
-                        }
-                    ])->whereHas('DesaDinas', function ($desaDinasQuery) use ($request) {
-                        $desaDinasQuery->with([
-                            'Kecamatan' => function ($kecamatanQuery) use ($request) {
-                                $kecamatanQuery->where('id', $request->id_kecamatan);
-                            }
-                        ])->whereHas('Kecamatan', function ($kecamatanQuery) use ($request) {
+            // LOKASI FILTER
+            if ($request->id_banjar_dinas) {
+                $griyaRumahs->whereHas('BanjarDinas', function ($banjarDinasQuery) use ($request) {
+                    $banjarDinasQuery->where('id', $request->id_banjar_dinas);
+                });
+                $sanggars->whereHas('BanjarDinas', function ($banjarDinasQuery) use ($request) {
+                    $banjarDinasQuery->where('id', $request->id_banjar_dinas);
+                });
+            } else if ($request->id_desa_dinas) {
+                $griyaRumahs->whereHas('BanjarDinas', function ($banjarDinasQuery) use ($request) {
+                    $banjarDinasQuery->whereHas('DesaDinas', function ($desaDinasQuery) use ($request) {
+                        $desaDinasQuery->where('id', $request->id_desa_dinas);
+                    });
+                });
+                $sanggars->whereHas('BanjarDinas', function ($banjarDinasQuery) use ($request) {
+                    $banjarDinasQuery->whereHas('DesaDinas', function ($desaDinasQuery) use ($request) {
+                        $desaDinasQuery->where('id', $request->id_desa_dinas);
+                    });
+                });
+            } else if ($request->id_kecamatan) {
+                $griyaRumahs->whereHas('BanjarDinas', function ($banjarDinasQuery) use ($request) {
+                    $banjarDinasQuery->whereHas('DesaDinas', function ($desaDinasQuery) use ($request) {
+                        $desaDinasQuery->whereHas('Kecamatan', function ($kecamatanQuery) use ($request) {
                             $kecamatanQuery->where('id', $request->id_kecamatan);
                         });
                     });
-                };
-                $griyaRumahs->with([
-                    'BanjarDinas' => $banjarDinasQuery,
-                ])->whereHas('BanjarDinas', $banjarDinasQuery);
-            } else {
-                $griyaRumahs->with(['BanjarDinas'])->whereHas('BanjarDinas');
+                });
+                $sanggars->whereHas('BanjarDinas', function ($banjarDinasQuery) use ($request) {
+                    $banjarDinasQuery->whereHas('DesaDinas', function ($desaDinasQuery) use ($request) {
+                        $desaDinasQuery->whereHas('Kecamatan', function ($kecamatanQuery) use ($request) {
+                            $kecamatanQuery->where('id', $request->id_kecamatan);
+                        });
+                    });
+                });
+            } else if ($request->id_kabupaten) {
+                $griyaRumahs->whereHas('BanjarDinas', function ($banjarDinasQuery) use ($request) {
+                    $banjarDinasQuery->whereHas('DesaDinas', function ($desaDinasQuery) use ($request) {
+                        $desaDinasQuery->whereHas('Kecamatan', function ($kecamatanQuery) use ($request) {
+                            $kecamatanQuery->whereHas('Kabupaten', function ($kabupatenQuery) use ($request) {
+                                $kabupatenQuery->where('id', $request->id_kabupaten);
+                            });
+                        });
+                    });
+                });
+                $sanggars->whereHas('BanjarDinas', function ($banjarDinasQuery) use ($request) {
+                    $banjarDinasQuery->whereHas('DesaDinas', function ($desaDinasQuery) use ($request) {
+                        $desaDinasQuery->whereHas('Kecamatan', function ($kecamatanQuery) use ($request) {
+                            $kecamatanQuery->whereHas('Kabupaten', function ($kabupatenQuery) use ($request) {
+                                $kabupatenQuery->where('id', $request->id_kabupaten);
+                            });
+                        });
+                    });
+                });
             }
 
-            $sanggars = $sanggars->get();
-            $griyaRumahs = $griyaRumahs->get();
+            // FILTER STATUS
+            if (isset($request->status)) {
+                if ($request->status == 'sulinggih') {
+                    $sanggars->where('id', -1);
+                    $griyaRumahs->whereHas('PemuputKarya', function ($pemuputKaryaQyery) use ($request, $user) {
+                        $pemuputKaryaQyery->where('tipe', 'sulinggih');
+                    });
+                } else if ($request->status == 'pemangku') {
+                    $sanggars->where('id', -1);
+                    $griyaRumahs->whereHas('PemuputKarya', function ($pemuputKaryaQyery) use ($request, $user) {
+                        $pemuputKaryaQyery->where('tipe', 'pemangku');
+                    });
+                } else if ($request->status == 'sanggar') {
+                    $griyaRumahs->where('id', -1);
+                }
+            }
+
+            if (isset($request->isFavorit)) {
+                if ($request->isFavorit == true) {
+                    $griyaRumahs->whereHas(
+                        'PemuputKarya',
+                        function ($pemuputkaryaQuery) use ($user) {
+                            $pemuputkaryaQuery->whereHas('FavoritUser', function ($favoritUserQuery) use ($user) {
+                                $favoritUserQuery->where('id_user', $user->id);
+                            });
+                        }
+                    );
+                    $griyaRumahs->with(
+                        'PemuputKarya',
+                        function ($pemuputkaryaQuery) use ($user) {
+                            $pemuputkaryaQuery->whereHas('FavoritUser', function ($favoritUserQuery) use ($user) {
+                                $favoritUserQuery->where('id_user', $user->id);
+                            });
+                        }
+                    );
+                    $sanggars->whereHas(
+                        'FavoritUser',
+                        function ($favoritUserQuery) use ($user) {
+                            $favoritUserQuery->where('id_user', $user->id);
+                        }
+                    );
+                    $sanggars->with(
+                        'FavoritUser',
+                        function ($favoritUserQuery) use ($user) {
+                            $favoritUserQuery->where('id_user', $user->id);
+                        }
+                    );
+                }
+            }
+
+            $griyaRumahResult = $griyaRumahs->get();
+            $sanggarsResult = $sanggars->get();
         } catch (ModelNotFoundException | PDOException | QueryException | \Throwable | \Exception $err) {
             return $err;
             return response()->json([
@@ -192,10 +178,10 @@ class KramaPemuputKaryaController extends Controller
         // RETURN
         return response()->json([
             'status' => 200,
-            'message' => 'Berhasil mengambil data pemuput  karya',
+            'message' => 'Berhasil mengambil data pemuput karya',
             'data' => [
-                'griya_rumahs' => $griyaRumahs,
-                'sanggars' => $sanggars
+                'griya_rumahs' => $griyaRumahResult,
+                'sanggars' => $sanggarsResult
             ],
         ], 200);
         // END
@@ -206,6 +192,7 @@ class KramaPemuputKaryaController extends Controller
         // SECURITY
         $validator = Validator::make($request->all(), [
             'id' => 'required|numeric',
+            'tipe_pemuput_karya' => 'required|in:sanggar,pemuput_karya'
         ]);
 
         if ($validator->fails()) {
@@ -221,18 +208,30 @@ class KramaPemuputKaryaController extends Controller
         try {
             $user = Auth::user();
             $isFavorit = true;
-            $favorite = Favorit::where('id_user', $user->id)->where('id_pemuput_karya', $request->id)->first();
-            if ($favorite == null) {
-                Favorit::create([
-                    'id_pemuput_karya' => $request->id,
-                    'id_user' => $user->id,
-                ]);
+            if ($request->tipe_pemuput_karya == 'pemuput_karya') {
+                $favorite = Favorit::where('id_user', $user->id)->where('id_pemuput_karya', $request->id)->first();
+                if ($favorite == null) {
+                    Favorit::create([
+                        'id_pemuput_karya' => $request->id,
+                        'id_user' => $user->id,
+                    ]);
+                } else {
+                    $favorite->delete();
+                    $isFavorit = false;
+                }
             } else {
-                $favorite->delete();
-                $isFavorit = false;
+                $favorite = Favorit::where('id_user', $user->id)->where('id_sanggar', $request->id)->first();
+                if ($favorite == null) {
+                    Favorit::create([
+                        'id_sanggar' => $request->id,
+                        'id_user' => $user->id,
+                    ]);
+                } else {
+                    $favorite->delete();
+                    $isFavorit = false;
+                }
             }
         } catch (ModelNotFoundException | PDOException | QueryException | \Throwable | \Exception $err) {
-            return $err;
             return response()->json([
                 'status' => 500,
                 'message' => 'Internal Server Error',
